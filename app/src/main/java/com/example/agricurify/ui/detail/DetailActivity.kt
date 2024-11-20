1aecedf519c983573e094160fa3f43400ba338a0
tanzz <bustanuldarking23@gmail.com>
2024-11-20 15:13:16 +0800

    DetailActivity

---
+++ app/src/main/java/com/example/agricurify/ui/detail/DetailActivity.kt
@@ -0,0 +1,17 @@
+package com.example.agricurify.ui.detail
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.agricurify.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_detail)
+
+    }
+}
