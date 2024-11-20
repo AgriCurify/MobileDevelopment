ade900b522a56a15ea67bf85f80046ef5b2df528
tanzz <bustanuldarking23@gmail.com>
2024-11-20 15:08:14 +0800

    menghapus yang tidak perlu, dan menambahkan fungsi hide supportActionBar

--- app/src/main/java/com/example/agricurify/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/agricurify/ui/profile/ProfileFragment.kt
@@ -1,10 +1,12 @@
 package com.example.agricurify.ui.profile
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.agricurify.databinding.FragmentProfileBinding
@@ -19,17 +21,21 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+    }
+
+    @SuppressLint("RestrictedApi")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (activity as AppCompatActivity?)?.supportActionBar?.apply {
+            setShowHideAnimationEnabled(false)
+            hide()
+        }
     }
 
     override fun onDestroyView() {
