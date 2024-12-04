b8587e3f0c4b57bf812b262d3598f0e08579a01b
tanzz <bustanuldarking23@gmail.com>
2024-12-04 17:16:32 +0800

    menambahkan apiservice untuk imageclassification

--- app/src/main/java/com/example/agricurify/data/di/Injection.kt
+++ app/src/main/java/com/example/agricurify/data/di/Injection.kt
@@ -3,11 +3,11 @@ package com.example.agricurify.data.di
 import android.content.Context
 import com.example.agricurify.data.remote.ApiConfig
 import com.example.agricurify.data.repository.MainRepository
-import com.google.android.gms.location.LocationServices
 
 object Injection {
     fun provideMainRepository(context: Context): MainRepository {
         val apiService = ApiConfig.getWeather()
-        return MainRepository.getInstance(apiService, context)
+        val apiServiceDetection = ApiConfig.imageClassification()
+        return MainRepository.getInstance(apiService, context, apiServiceDetection)
     }
 }
