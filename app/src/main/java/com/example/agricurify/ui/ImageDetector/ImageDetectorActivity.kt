fff960d1d29ef8b770e010019127c249bab5d8a8
tanzz <bustanuldarking23@gmail.com>
2024-12-04 17:16:13 +0800

    menambahkan kode untuk upload image ke api

--- app/src/main/java/com/example/agricurify/ui/ImageDetector/ImageDetectorActivity.kt
+++ app/src/main/java/com/example/agricurify/ui/ImageDetector/ImageDetectorActivity.kt
@@ -4,22 +4,33 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.OpenableColumns
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
-import com.example.agricurify.MainActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.agricurify.R
+import com.example.agricurify.data.response.ModelResponse
 import com.example.agricurify.databinding.ActivityImageDetectorBinding
 import com.example.agricurify.ui.result.ResultActivity
+import com.example.agricurify.ui.viewmodel.MainViewModel
+import com.example.agricurify.ui.viewmodel.ViewModelFactory
 import com.example.agricurify.utils.getImageUri
+import com.example.agricurify.utils.reduceFileImage
+import com.example.agricurify.utils.uriToFile
+import kotlinx.coroutines.launch
 
 class ImageDetectorActivity : AppCompatActivity() {
     private lateinit var binding: ActivityImageDetectorBinding
-    private lateinit var viewModel: ImageDetectorViewModel
+    private val viewModel by viewModels<MainViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
+
+    private var currentImageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,72 +42,84 @@ class ImageDetectorActivity : AppCompatActivity() {
             finish()
         }
 
-        viewModel = ViewModelProvider(this)[ImageDetectorViewModel::class.java]
-
-        setupListeners()
-        observeViewModel()
-    }
-
-    private fun setupListeners() {
         binding.clipButton.setOnClickListener { startGallery() }
         binding.cameraButton.setOnClickListener { startCamera() }
         binding.btnScan.setOnClickListener {
-            viewModel.currentImageUri.value?.let { uri ->
-                viewModel.classifyImage(uri)
-            } ?: showToast(getString(R.string.empty_image_warning))
+            lifecycleScope.launch {
+                uploadImage()
+            }
+        }
+
+        viewModel.isLoading.observe(this){
+            showLoading(it)
         }
     }
 
-    private fun observeViewModel() {
-        viewModel.currentImageUri.observe(this) { uri ->
-            uri?.let {
-                binding.previewImageView.setImageURI(it)
-                binding.tvPreview.text = getFileNameFromUri(it)
-            }
-        }
-
-        viewModel.classificationResult.observe(this) { result ->
-            result?.let { (label, score) ->
-                moveToResult(label, score)
-            }
-        }
-
-        viewModel.isLoading.observe(this) { isLoading ->
-            binding.progressIndicator.visibility = if (isLoading) View.VISIBLE else View.GONE
-        }
-
-        viewModel.error.observe(this) { error ->
-            error?.let {
-                showToast(it)
-                viewModel.clearError()
-            }
-        }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(IMAGE_KEY, currentImageUri)
     }
 
-    private fun moveToResult(label: String, score: String) {
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        @Suppress("DEPRECATION")
+        currentImageUri = savedInstanceState.getParcelable(IMAGE_KEY)
+        showImage()
+    }
+
+    private suspend fun uploadImage() {
+        currentImageUri?.let { uri ->
+            try {
+                val imageFile = uriToFile(uri, this).reduceFileImage()
+                Log.d("Image File Path", "File Path: ${imageFile.path}, Exists: ${imageFile.exists()}")
+                showLoading(true)
+
+                viewModel.uploadAppleImage(imageFile)
+                viewModel.modelData.observe(this) { response ->
+                    response?.let {
+                        Log.d("ImageDetector", "Response: $response")
+                        moveToResultActivity(it)
+
+                    }
+                    showLoading(false)
+                }
+
+            } catch (e: Exception) {
+                Log.e("Upload Error", "Error preparing file for upload: ${e.message}")
+            } finally {
+                showLoading(false)
+            }
+        } ?: showToast(getString(R.string.empty_image_warning))
+    }
+
+
+
+    private fun moveToResultActivity(response: ModelResponse) {
         val intent = Intent(this, ResultActivity::class.java).apply {
-            putExtra(ResultActivity.EXTRA_LABEL, label)
-            putExtra(ResultActivity.EXTRA_SCORE, score)
-            putExtra(ResultActivity.EXTRA_IMAGE_URI, viewModel.currentImageUri.value.toString())
+            putExtra(ResultActivity.EXTRA_IMAGE_URI, currentImageUri.toString())
+            putExtra(ResultActivity.EXTRA_MODEL_RESPONSE, response)
+
         }
         startActivity(intent)
     }
 
     private fun startCamera() {
-        val (uri, _) = getImageUri(this)
-        viewModel.setCurrentImageUri(uri)
-        launcherIntentCamera.launch(uri)
+        currentImageUri = getImageUri(this)
+        launcherIntentCamera.launch(currentImageUri!!)
     }
 
     private val launcherIntentCamera = registerForActivityResult(
         ActivityResultContracts.TakePicture()
     ) { isSuccess ->
         if (isSuccess) {
-            viewModel.currentImageUri.value?.let {
-                showImage()
+            showImage()
+            currentImageUri?.let { uri ->
+                binding.tvPreview.text = getFileNameFromUri(uri)
             }
+
         } else {
-            viewModel.setCurrentImageUri(null)
+            currentImageUri = null
         }
     }
 
@@ -109,10 +132,14 @@ class ImageDetectorActivity : AppCompatActivity() {
     private val launchGallery = registerForActivityResult(
         ActivityResultContracts.PickVisualMedia()
     ) { uri: Uri? ->
-        uri?.let {
-            viewModel.setCurrentImageUri(it)
+        if (uri != null) {
+            currentImageUri = uri
             showImage()
-        } ?: showToast(getString(R.string.empty_image_warning))
+            binding.tvPreview.text = getFileNameFromUri(uri)
+        } else {
+            showToast(getString(R.string.empty_image_warning))
+
+        }
     }
 
     private fun getFileNameFromUri(uri: Uri): String {
@@ -133,9 +160,19 @@ class ImageDetectorActivity : AppCompatActivity() {
     }
 
     private fun showImage() {
-        viewModel.currentImageUri.value?.let {
+        currentImageUri?.let {
+            Log.d("Imgae Uri", "ShowImage: $it")
             binding.previewImageView.setImageURI(it)
         }
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressIndicator.visibility = if (isLoading) View.VISIBLE else View.GONE
+
+    }
+
+    companion object {
+        private const val IMAGE_KEY = "currentImageUri"
     }
 
 }
