8f8d479959ab964fc284c29e10143683d621161b
tanzz <bustanuldarking23@gmail.com>
2024-12-04 17:18:26 +0800

    menampilkan result dari modelresponse

--- app/src/main/java/com/example/agricurify/ui/result/ResultActivity.kt
+++ app/src/main/java/com/example/agricurify/ui/result/ResultActivity.kt
@@ -3,13 +3,16 @@ package com.example.agricurify.ui.result
 import android.annotation.SuppressLint
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.agricurify.R
+import com.example.agricurify.data.response.ModelResponse
 import com.example.agricurify.databinding.ActivityResultBinding
 
+@Suppress("DEPRECATION")
 class ResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityResultBinding
 
@@ -21,17 +24,40 @@ class ResultActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
-        val label = intent.getStringExtra(EXTRA_LABEL)
-        val score = intent.getStringExtra(EXTRA_SCORE)
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
+
+
         val imageString = intent.getStringExtra(EXTRA_IMAGE_URI)
         imageString?.let {
             val imageUri = Uri.parse(it)
             binding.previewImageView.setImageURI(imageUri)
         }
+        val modelResponse = intent.getParcelableExtra<ModelResponse>(EXTRA_MODEL_RESPONSE)
+        if (modelResponse != null) {
+            displayResult(modelResponse)
+        }
 
-        binding.tvName.text = label
-        binding.tvScore.text = "Akurasi : $score"
+    }
 
+    @SuppressLint("SetTextI18n")
+    private fun displayResult(response: ModelResponse) {
+        binding.tvName.text = response.diseaseInfo.name
+        binding.tvScore.text = "Akurasi : ${response.confidence}%"
+        binding.tvDescription.text = response.diseaseInfo.description
+
+        val treatmentList = response.diseaseInfo.treatment
+        val treatmentText = StringBuilder("")
+        for ((index, treatment) in treatmentList.withIndex()) {
+            treatmentText.append("${index + 1}. $treatment\n")
+        }
+        binding.tvTreatment.text = treatmentText.toString()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("ResultActivity", "Destroyed")
     }
 
 
@@ -39,6 +65,7 @@ class ResultActivity : AppCompatActivity() {
         const val EXTRA_LABEL = "extra_label"
         const val EXTRA_SCORE = "extra_score"
         const val EXTRA_IMAGE_URI = "extra_image_uri"
+        const val EXTRA_MODEL_RESPONSE = "extra_model_response"
 
     }
 }
