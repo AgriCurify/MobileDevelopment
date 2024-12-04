01a7902925e160f3e56ed82d22dc0c79ce9de3be
tanzz <bustanuldarking23@gmail.com>
2024-12-04 17:09:22 +0800

    base url ke api dari cc

--- app/src/main/java/com/example/agricurify/data/remote/ApiConfig.kt
+++ app/src/main/java/com/example/agricurify/data/remote/ApiConfig.kt
@@ -4,6 +4,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiConfig {
     fun getWeather(): ApiService{
@@ -19,4 +20,22 @@ object ApiConfig {
             .build()
         return retrofit.create(ApiService::class.java)
     }
+
+    fun imageClassification(): ApiService{
+        val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS) // Timeout koneksi 30 detik
+            .readTimeout(30, TimeUnit.SECONDS) // Timeout baca 30 detik
+            .writeTimeout(30, TimeUnit.SECONDS) // Timeout tulis 30 detik
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://agricurify-c242-ps224.et.r.appspot.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
+
 }
