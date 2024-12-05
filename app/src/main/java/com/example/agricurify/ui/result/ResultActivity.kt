4d332ffb0ee16177975f764950a8f0934d5d50bf
tanzz <bustanuldarking23@gmail.com>
2024-12-05 20:51:51 +0800

    inisialisasi library mlkit translate

--- app/src/main/java/com/example/agricurify/ui/result/ResultActivity.kt
+++ app/src/main/java/com/example/agricurify/ui/result/ResultActivity.kt
@@ -4,9 +4,15 @@ import android.annotation.SuppressLint
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.agricurify.data.response.ModelResponse
 import com.example.agricurify.databinding.ActivityResultBinding
+import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.nl.translate.TranslateLanguage
+import com.google.mlkit.nl.translate.Translation
+import com.google.mlkit.nl.translate.TranslatorOptions
 
 @Suppress("DEPRECATION")
 class ResultActivity : AppCompatActivity() {
@@ -30,11 +36,20 @@ class ResultActivity : AppCompatActivity() {
             binding.previewImageView.setImageURI(imageUri)
         }
         val modelResponse = intent.getParcelableExtra<ModelResponse>(EXTRA_MODEL_RESPONSE)
-        if (modelResponse != null) {
-            displayResult(modelResponse)
+        modelResponse?.let {
+            displayResult(it)
+        }
+
+        binding.btnTranslate.setOnClickListener {
+            modelResponse?.let { response ->
+                binding.progressBar.visibility = View.VISIBLE
+                translateContent(response)
+            }
         }
 
     }
+
+
 
     @SuppressLint("SetTextI18n")
     private fun displayResult(response: ModelResponse) {
@@ -48,6 +63,64 @@ class ResultActivity : AppCompatActivity() {
             treatmentText.append("${index + 1}. $treatment\n")
         }
         binding.tvTreatment.text = treatmentText.toString()
+
+    }
+
+    private fun translateContent(response: ModelResponse) {
+        translateText(response.diseaseInfo.name) { translatedName ->
+            binding.tvName.text = translatedName
+        }
+
+        translateText(response.diseaseInfo.description) { translatedDescription ->
+            binding.tvDescription.text = translatedDescription
+        }
+
+        val treatmentList = response.diseaseInfo.treatment
+        val treatmentText = StringBuilder("")
+        treatmentList.forEachIndexed { index, treatment ->
+            translateText(treatment) { translatedTreatment ->
+                treatmentText.append("${index + 1}. $translatedTreatment\n")
+                binding.tvTreatment.text = treatmentText.toString()
+            }
+        }
+    }
+
+    private fun translateText(inputText: String, callback: (String) -> Unit) {
+        val options = TranslatorOptions.Builder()
+            .setSourceLanguage(TranslateLanguage.ENGLISH)
+            .setTargetLanguage(TranslateLanguage.INDONESIAN)
+            .build()
+        val indonesianEnglishTranslator = Translation.getClient(options)
+
+        val conditions = DownloadConditions.Builder()
+            .requireWifi()
+            .build()
+
+        indonesianEnglishTranslator.downloadModelIfNeeded(conditions)
+            .addOnSuccessListener {
+                indonesianEnglishTranslator.translate(inputText)
+                    .addOnSuccessListener { translatedText ->
+                        callback(translatedText)
+                        indonesianEnglishTranslator.close()
+                        binding.progressBar.visibility = View.GONE
+
+                    }
+                    .addOnFailureListener { exception ->
+                        showToast("Translation failed: ${exception.message}")
+                        indonesianEnglishTranslator.close()
+                        binding.progressBar.visibility = View.GONE
+                    }
+            }
+            .addOnFailureListener { exception ->
+                showToast("Model download failed: ${exception.message}")
+                indonesianEnglishTranslator.close()
+                binding.progressBar.visibility = View.GONE
+            }
+    }
+
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
 
