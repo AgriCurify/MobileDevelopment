a34381f8b2af39d90b10055d1668d95891b3781f
tanzz <bustanuldarking23@gmail.com>
2024-12-09 01:15:13 +0800

    menambahkan injection untuk apiAuthentication dan preference

--- app/src/main/java/com/example/agricurify/data/di/Injection.kt
+++ app/src/main/java/com/example/agricurify/data/di/Injection.kt
@@ -1,6 +1,8 @@
 package com.example.agricurify.data.di
 
 import android.content.Context
+import com.example.agricurify.data.preference.Preference
+import com.example.agricurify.data.preference.dataStore
 import com.example.agricurify.data.remote.ApiConfig
 import com.example.agricurify.data.repository.MainRepository
 
@@ -8,6 +10,12 @@ object Injection {
     fun provideMainRepository(context: Context): MainRepository {
         val apiService = ApiConfig.getWeather()
         val apiServiceDetection = ApiConfig.imageClassification()
-        return MainRepository.getInstance(apiService, context, apiServiceDetection)
+        val apiAuthentication = ApiConfig.authentication()
+        val pref = Preference.getInstance(context.dataStore)
+        return MainRepository.getInstance(apiService, context, apiServiceDetection, apiAuthentication, pref)
+    }
+
+    fun userPreference(context: Context): Preference {
+        return Preference.getInstance(context.dataStore)
     }
 }
