812f03ebbbd489c024200196cfc9c6cbe3f6e640
tanzz <bustanuldarking23@gmail.com>
2024-12-09 01:24:12 +0800

    menambahkan preference sebagai parameter

--- app/src/main/java/com/example/agricurify/ui/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/example/agricurify/ui/viewmodel/ViewModelFactory.kt
@@ -4,17 +4,19 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.agricurify.data.di.Injection
+import com.example.agricurify.data.preference.Preference
 import com.example.agricurify.data.repository.MainRepository
 
 @Suppress("UNCHECKED_CAST")
 class ViewModelFactory(
-    private val mRepository: MainRepository
+    private val mRepository: MainRepository,
+    private val mPreference: Preference
 ) : ViewModelProvider.NewInstanceFactory() {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
             modelClass.isAssignableFrom(MainViewModel::class.java) -> {
-                MainViewModel(mRepository) as T
+                MainViewModel(mRepository, mPreference) as T
             }
             else -> throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
         }
@@ -27,7 +29,8 @@ class ViewModelFactory(
         fun getInstance(context: Context): ViewModelFactory =
             instance ?: synchronized(this){
                 instance ?: ViewModelFactory(
-                    Injection.provideMainRepository(context)
+                    Injection.provideMainRepository(context),
+                    Injection.userPreference(context)
                 )
             }.also { instance = it }
     }
