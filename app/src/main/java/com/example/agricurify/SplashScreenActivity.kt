1e32fd76a034f0e052d04cc4259f91b592e49838
tanzz <bustanuldarking23@gmail.com>
2024-12-09 01:23:31 +0800

    penambahan kondisi untuk pengecekan token, jika ada maka ke main activity, jika tidak ada maka ke login activity

--- app/src/main/java/com/example/agricurify/SplashScreenActivity.kt
+++ app/src/main/java/com/example/agricurify/SplashScreenActivity.kt
@@ -4,16 +4,23 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.animation.AnimationUtils
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.agricurify.databinding.ActivitySplashScreenBinding
+import com.example.agricurify.ui.viewmodel.MainViewModel
+import com.example.agricurify.ui.viewmodel.ViewModelFactory
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashScreenBinding
+    private val viewModel by viewModels<MainViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,13 +29,19 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
 
+
         val fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
         binding.logoImage.startAnimation(fadeIn)
 
         lifecycleScope.launch {
             delay(2000)
-            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
-            startActivity(intent)
+            val token = viewModel.getToken().first()
+
+            if (token.isNotEmpty()){
+                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            } else {
+                startActivity(Intent(this@SplashScreenActivity, LoginActivity::class.java))
+            }
             finish()
         }
     }
