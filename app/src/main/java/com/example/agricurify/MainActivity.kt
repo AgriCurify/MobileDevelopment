c42f0e72f22091326c368188dd691f31f7692622
Firman <73871010+FirmanAdit24@users.noreply.github.com>
2024-12-09 07:42:03 +0800

    memindahkan onboarding ke login

--- app/src/main/java/com/example/agricurify/MainActivity.kt
+++ app/src/main/java/com/example/agricurify/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.example.agricurify
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.WindowInsets
-import android.view.WindowManager
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.agricurify.databinding.ActivityMainBinding
+import com.example.agricurify.ui.imageDetector.ImageDetectorActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.agricurify.databinding.ActivityMainBinding
-import com.example.agricurify.ui.imageDetector.ImageDetectorActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,40 +17,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // Cek apakah ini pertama kali aplikasi dibuka
-        val preferences = getSharedPreferences("AppPrefs", MODE_PRIVATE)
-        val isFirstRun = preferences.getBoolean("isFirstRun", true)
-
-        if (isFirstRun) {
-            // Jika pertama kali, tampilkan OnboardingActivity
-            val intent = Intent(this, OnBoardingActivity::class.java)
-            startActivity(intent)
-            finish()  // Tutup MainActivity
-        } else {
-            // Jika bukan pertama kali, lanjutkan ke MainActivity
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-
-            val navView: BottomNavigationView = binding.navView
-            val navController = findNavController(R.id.nav_host_fragment_activity_main)
-            val appBarConfiguration = AppBarConfiguration(
-                setOf(
-                    R.id.navigation_home, R.id.navigation_profile
-                )
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_profile
             )
-            setupActionBarWithNavController(navController, appBarConfiguration)
-            navView.setupWithNavController(navController)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
-            binding.cameraScan.setOnClickListener {
+        binding.cameraScan.setOnClickListener {
             val intent = Intent(this, ImageDetectorActivity::class.java)
             startActivity(intent)
-            }
         }
-
-        // Set flag supaya Onboarding tidak muncul lagi di masa depan
-        val editor = preferences.edit()
-        editor.putBoolean("isFirstRun", false)
-        editor.apply()
     }
 }
