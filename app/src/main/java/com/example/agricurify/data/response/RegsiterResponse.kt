cb69a4bd90b0ac07f4b43d2468b99fe656bdacdc
tanzz <bustanuldarking23@gmail.com>
2024-12-09 01:18:04 +0800

    response dan request untuk api service login register. request digunakan untuk menampung data yang menjadi request api yang content-type raw/json

---
+++ app/src/main/java/com/example/agricurify/data/response/RegsiterResponse.kt
@@ -0,0 +1,9 @@
+package com.example.agricurify.data.response
+
+import com.google.gson.annotations.SerializedName
+
+data class RegsiterResponse(
+
+	@field:SerializedName("message")
+	val message: String
+)
