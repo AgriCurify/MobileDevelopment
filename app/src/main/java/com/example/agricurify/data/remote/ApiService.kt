c7025d540eb9ed7859f889fac8dcb72a9d97ebfc
Firman <73871010+FirmanAdit24@users.noreply.github.com>
2024-12-09 12:47:27 +0800

    menambahkan  logout, dan update user

--- app/src/main/java/com/example/agricurify/data/remote/ApiService.kt
+++ app/src/main/java/com/example/agricurify/data/remote/ApiService.kt
@@ -2,15 +2,21 @@ package com.example.agricurify.data.remote
 
 import com.example.agricurify.data.response.LoginRequest
 import com.example.agricurify.data.response.LoginResponse
+import com.example.agricurify.data.response.LogoutResponse
 import com.example.agricurify.data.response.ModelResponse
+import com.example.agricurify.data.response.ProfileImageResponse
 import com.example.agricurify.data.response.RegisterRequest
 import com.example.agricurify.data.response.RegsiterResponse
+import com.example.agricurify.data.response.UserProfileResponse
 import com.example.agricurify.data.response.WeatherResponse
 import okhttp3.MultipartBody
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Part
 import retrofit2.http.Query
 
@@ -46,5 +52,22 @@ interface ApiService {
     @POST("login")
     suspend fun login(@Body request: LoginRequest): LoginResponse
 
+    @GET("users")
+    suspend fun getProfile(@Header("Authorization") token: String): UserProfileResponse
 
+    @POST("logout")
+    suspend fun logout(@Header("Authorization") token: String): LogoutResponse
+
+    @Multipart
+    @PUT("/users/updateProfile")
+    suspend fun updateProfileImage(
+        @Header("Authorization") token: String,
+        @Part profileImage: MultipartBody.Part
+    ): Response<ProfileImageResponse>
+
+    @PUT("users")
+    suspend fun updateUserData(
+        @Header("Authorization") token: String,
+        @Body updateRequest: Map<String, String>
+    ): Response<UserProfileResponse>
 }
